4195803609c6dbd02e19adea37e3e03139a52ab9
Brian <brianporras6019@gmail.com>
2026-01-12 14:56:11 -0600

    first commit

---
+++ Person.java
@@ -0,0 +1,36 @@
+public class Person {
+
+    private String name;
+    private int age;
+    private Pet[] pets;
+
+    public Person(String name, int age, int numbersOfPets) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Pet[] getPets() {
+        return pets;
+    }
+
+    public void setPets(Pet[] pets) {
+        this.pets = pets;
+    }
+
+}
